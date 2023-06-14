b05dd23c314c4d453f35ea8876541efd5c83f24f
Yogesh More <yogesh.more2098@gmail.com>
2023-06-14 16:30:48 +0530

    Added: Welcome message.

---
+++ src/main/java/com/employeepayrollapplication/EmployeePayrollApplication.java
@@ -0,0 +1,14 @@
+package com.employeepayrollapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeePayrollApplication.class, args);
+        System.out.println(" ********** Welcome to Employee payroll application ********** ");
+    }
+
+}
